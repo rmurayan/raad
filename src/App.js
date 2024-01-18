bd57c3cbcfea9091c38b40fe5f222316af83a636
Raid Murayan <raidmurayan@gmail.com>
2024-01-18 09:43:02 -0500

    edit the app js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App Developed {name }by Raid Murayan.
+          App Developed {name }by Raid Murayan!.
         </p>
         <a
           className="App-link"
